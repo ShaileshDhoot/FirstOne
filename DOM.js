0185fb3d20b1dcc2b5e9667c2d294acdb8eed6e2
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-03-20 14:12:28 +0530

    new change with for each

--- DOM.js
+++ DOM.js
@@ -19,22 +19,30 @@ console.log(header1);
 // console.log(mainTitle.textContent);
 // mainTitle.style.border = "solid 2px #000"
 
-const items = document.querySelectorAll(".list-group-item")
+// const items = document.querySelectorAll(".list-group-item")
 // console.log(items);
 
 // console.log(items[2]);
 // console.log(items[0]);
 // items[1].textContent = "Hello 2"
 // items[1].style.fontWeight = "bold"
-items[2].style.backgroundColor = "green"
+// items[2].style.backgroundColor = "green"
 // const lastItem = document.querySelector(".list-group-item:last-child");
 // lastItem.style.fontWeight='bold'
 
 
-// items.forEach(()=>{
-//       items.style.fontWeight = "bold"
-// })
-for(let i = 0; i<items.length;i++){
-    items[i].style.fontWeight = "bold"
-    items[i].style.color = "blue"
-}
+// for(let i = 0; i<items.length;i++){
+//     items[i].style.fontWeight = "bold"
+//     items[i].style.color = "blue"
+// }
+
+//have to change it let to modify array 
+let items = document.querySelectorAll(".list-group-item")
+items=Array.from(items)
+items.forEach((item)=>{
+      item.style.fontWeight = "bold"
+})
+
+
+
+// aah finally it worked - remebet to add varible name in parameter and use that name as object to make the changes
