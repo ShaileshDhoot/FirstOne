85018fb4bc7b7b0a302ae71f3745435fcc7af275
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-03-20 12:57:59 +0530

    adding secon file

---
+++ DOM.js
@@ -0,0 +1,21 @@
+console.dir(document);
+console.log(document.head)
+console.log(document.body.children);
+const header1 = document.querySelector("#main-header")
+console.log(header1.textContent);
+// header1.textContent= "DOM-MANUPULATION"
+
+
+
+// Make the title have a black border as the youtuber does
+
+const title = document.querySelector(".title")
+console.log(title);
+// title.style.border = "solid 3px "
+title.style.color = "green"
+title.style.border = "solid 3px "
+
+const mainTitle = document.querySelector("#header-title")
+console.log(mainTitle.textContent);
+mainTitle.style.border = "solid 2px #000"
+
