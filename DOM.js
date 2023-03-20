db09373c3befcede50a91a20acb3127a818937ca
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-03-20 13:04:43 +0530

    new file

--- DOM.js
+++ DOM.js
@@ -1,21 +1,21 @@
-console.dir(document);
-console.log(document.head)
-console.log(document.body.children);
+// console.dir(document);
+// console.log(document.head)
+// console.log(document.body.children);
 const header1 = document.querySelector("#main-header")
-console.log(header1.textContent);
-// header1.textContent= "DOM-MANUPULATION"
+console.log(header1);
+// // header1.textContent= "DOM-MANUPULATION"
 
 
 
-// Make the title have a black border as the youtuber does
+// // Make the title have a black border as the youtuber does
 
-const title = document.querySelector(".title")
-console.log(title);
+// const title = document.querySelector(".title")
+// console.log(title);
+// // title.style.border = "solid 3px "
+// title.style.color = "green"
 // title.style.border = "solid 3px "
-title.style.color = "green"
-title.style.border = "solid 3px "
 
-const mainTitle = document.querySelector("#header-title")
-console.log(mainTitle.textContent);
-mainTitle.style.border = "solid 2px #000"
+// const mainTitle = document.querySelector("#header-title")
+// console.log(mainTitle.textContent);
+// mainTitle.style.border = "solid 2px #000"
 
