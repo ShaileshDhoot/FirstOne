ef436ba3a7f25ea60d4826bc170ed253511756af
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-03-20 14:53:42 +0530

    make forreach work, task - 6

--- DOM.js
+++ DOM.js
@@ -1,8 +1,8 @@
 // console.dir(document);
 // console.log(document.head)
 // console.log(document.body.children);
-const header1 = document.querySelector("#main-header")
-console.log(header1);
+// const header1 = document.querySelector("#main-header")
+// console.log(header1);
 // // header1.textContent= "DOM-MANUPULATION"
 
 
@@ -27,6 +27,7 @@ console.log(header1);
 // items[1].textContent = "Hello 2"
 // items[1].style.fontWeight = "bold"
 // items[2].style.backgroundColor = "green"
+
 // const lastItem = document.querySelector(".list-group-item:last-child");
 // lastItem.style.fontWeight='bold'
 
@@ -37,12 +38,31 @@ console.log(header1);
 // }
 
 //have to change it let to modify array 
-let items = document.querySelectorAll(".list-group-item")
-items=Array.from(items)
-items.forEach((item)=>{
-      item.style.fontWeight = "bold"
-})
+// let items = document.querySelectorAll(".list-group-item")
+// items=Array.from(items)
+// items.forEach((item)=>{
+//       item.style.fontWeight = "bold"
+// })
 
 
 
 // aah finally it worked - remebet to add varible name in parameter and use that name as object to make the changes
+
+
+// Make the 2nd item have green background color
+// Make the 3rd item invisible
+
+// const items = document.querySelectorAll(".list-group-item")
+// items[1].style.backgroundColor = "green"
+// items[2].style.display = "none"
+
+// Using QuerySelectorALL change the font color to green for 2nd item in the item list
+// Choose all the odd elements and make their background green using QuerySelectorALL﻿
+
+// const items = document.getElementsByClassName("list-group-item")
+// items[1].style.color = "blue"
+
+let oddItems = document.querySelectorAll("li:nth-child(odd)");
+oddItems.forEach((item)=>{
+      item.style.backgroundColor = "green"
+})
